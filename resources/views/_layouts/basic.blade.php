d95791778444b2319abbe1bc638c4585d65f312a
Jason Polito <jasonpolito@gmail.com>
2025-07-14 18:13:33 -0400

    deploy

--- resources/views/_layouts/basic.blade.php
+++ resources/views/_layouts/basic.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('meta_title', 'jasonpolito.github.io')</title>
+    <meta name="description" content="@yield('meta_description', 'jasonpolito.github.io')">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,5 +1,9 @@
 @extends('_layouts.basic')
 
+@section('meta_title', 'Test Page - jasonpolito.github.io')
+@section('meta_description', 'This is a test page to ensure that the static site generation works correctly with Laravel
+    and Vite.')
+
 @section('content')
     <h1>Testing a <x-gradient-text>sub page.</x-gradient-text></h1>
     <p>
