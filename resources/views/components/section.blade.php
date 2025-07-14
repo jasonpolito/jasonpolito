acc7f54a5eb1f8ba5dd14ae5587ef02ee0b208e2
Jason Polito <jasonpolito@gmail.com>
2025-07-14 16:25:13 -0400

    testing scripts

---
+++ resources/views/components/section.blade.php
@@ -0,0 +1,5 @@
+<section {{ $attributes->merge(['class' => 'py-4 md:py-8 xl:py-16']) }}>
+    <x-container>
+        {{ $slot }}
+    </x-container>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
-    <meta name="description" content="A simple Laravel application">
+    <title>Jason Polito</title>
+    <meta name="description" content="">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
 <body>
-    hello world
+    <x-section>
+        <h1 class="text-4xl font-bold">Testing static site generation with Laravel.</h1>
+    </x-section>
 </body>
 
 </html>
